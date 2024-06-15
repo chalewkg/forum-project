3452ca646ebb837992e36c593019ffa130dccf2b
chalewkg <chalewkg@gmail.com>
2024-06-14 20:12:39 -0400

    url updated

--- Frontend/src/axiosConfig.js
+++ Frontend/src/axiosConfig.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const axiosBase = axios.create({
-    baseURL: 'http://localhost:5500/api'
-    // baseURL: 'https://economic-apricot-wire.glitch.me'
-})
+  // baseURL: 'http://localhost:5500/api'
+  // baseURL: 'https://economic-apricot-wire.glitch.me'
+
+  baseURL: "http://3.140.186.242:5500/api",
+});
 
 export default axiosBase;
