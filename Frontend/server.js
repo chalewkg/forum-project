f6e8ecc36d55ef532e98c7ff539d400090327e7a
chalewkg <chalewkg@gmail.com>
2024-06-16 01:12:14 -0400

    server port updated

--- Frontend/server.js
+++ Frontend/server.js
@@ -12,8 +12,8 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
-// Listen on port 80
-const PORT = process.env.PORT || 80;
+// Listen on port 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
