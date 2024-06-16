172c8f862dd996b62a8f1024727eb4bab61d4565
chalewkg <chalewkg@gmail.com>
2024-06-16 12:35:39 -0400

    serve and app.js udpated

--- Frontend/server.js
+++ Frontend/server.js
@@ -1,3 +1,23 @@
+// const express = require("express");
+// const path = require("path");
+
+// // Create an express app
+// const app = express();
+
+// // Serve the static files from the React app
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// // Redirect all requests to the index.html file
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+// });
+
+// // Listen on port 3000
+// const PORT = process.env.PORT || 5500;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
 const express = require("express");
 const path = require("path");
 
@@ -5,15 +25,15 @@ const path = require("path");
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 // Redirect all requests to the index.html file
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // Listen on port 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
