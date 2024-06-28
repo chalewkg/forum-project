c1aa0da45596e4fb090710b444548aac3a2d267b
chalewkg <chalewkg@gmail.com>
2024-06-28 18:54:34 -0400

    ip adderss updated

--- Frontend/src/axiosConfig.js
+++ Frontend/src/axiosConfig.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "http://localhost:5500/api",
+  // baseURL: "http://localhost:5500/api",
   // baseURL: 'https://economic-apricot-wire.glitch.me'
 
-  // baseURL: "http://evngadiforum.com:5500/api",
+  baseURL: "http://evngadiforum.com:5500/api",
 });
 
 export default axiosBase;
